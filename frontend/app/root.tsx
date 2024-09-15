399248fa719dd9846536fed6e1c19906e1d673be
chell-uoxou <chelluoxou@gmail.com>
2024-09-16 07:01:15 +0900

    update shadcn ui コンポーネントを追加

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -7,7 +7,7 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -16,6 +16,7 @@ export const links: LinksFunction = () => [
     href: "https://fonts.gstatic.com",
     crossOrigin: "anonymous",
   },
+  { rel: "stylesheet", href: styles },
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",

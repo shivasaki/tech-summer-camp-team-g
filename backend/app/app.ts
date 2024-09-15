602626438ae5c0a589e8b1ab4a54d5f899e2ed92
YoujinTea <takeanhime0126@gmail.com>
2024-09-16 01:08:10 +0900

    expressやろうね

--- backend/app/app.ts
+++ backend/app/app.ts
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/", (req, res) => {
+  res.send("Hello World! This is post!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });

21961377551721480897e07c4182e2a510e1a6e9
YoujinTea <takeanhime0126@gmail.com>
2024-09-16 08:55:35 +0900

    /rollにトークンの認証を追加

--- backend/app/router/roll.ts
+++ backend/app/router/roll.ts
@@ -7,24 +7,49 @@ const router = Router();
 
 const rollRouter = router.post("/roll", async (req, res: Response) => {
   try {
-    const tokenId = "467dfc0b-aeb0-424e-98c0-e63b0004f7f2";
-    console.log(tokenId);
+    const authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+      // トークンを取り出す
+      const token = authHeader.split(' ')[1];
+      console.log(token);
 
-    const requestedAt = new Date();
+      // トークンの有効性を確認
+      const tokenResult = await query(
+        `SELECT * FROM token WHERE token = $1`,
+        [token, new Date()]
+      );
 
-    // 新しいセッションを作成
-    const result = await query(
-      `INSERT INTO roll_session (id, token_id, requested_at) VALUES ($1, $2, $3) RETURNING id`,
-      [uuid(), tokenId, requestedAt]
-    );
-    console.log(result);
-    const sessionId = result.rows[0].id;
+      const tokenExpired = await query(
+        `SELECT * FROM token WHERE token = $1 AND expired_at > $2`,
+        [token, new Date()]
+      );
 
-    // ロールジョブをキューに追加
-    addRollJob(sessionId);
+      if (tokenResult.rows.length === 0) {
+        return res.status(404).json({ error: "Token not found" });
+      }
+      if (tokenExpired.rows.length === 0) {
+        return res.status(403).json({ error: "Token expired" });
+      }
 
-    // セッションIDを返却
-    res.status(201).json({ sessionId, tokenId });
+      const requestedAt = new Date();
+
+      // 新しいセッションを作成
+      const result = await query(
+        `INSERT INTO roll_session (id, token_id, requested_at) VALUES ($1, $2, $3) RETURNING id`,
+        [uuid(), token, requestedAt]
+      );
+      console.log(result);
+      const sessionId = result.rows[0].id;
+
+      // ロールジョブをキューに追加
+      addRollJob(sessionId);
+
+      // セッションIDを返却
+      res.status(201).json({ sessionId });
+    }
+    else {
+      res.status(401).json({ error: "Unauthorized" });
+    }
   } catch (error) {
     console.error("Error creating roll session:", error);
     res.status(500).json({ error: "Internal server error" });

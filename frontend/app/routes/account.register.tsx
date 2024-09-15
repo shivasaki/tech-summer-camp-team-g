442093096160ff500905b5071194705c0cd317e2
chell-uoxou <chelluoxou@gmail.com>
2024-09-16 07:02:33 +0900

    feat registerをマークアップ

--- frontend/app/routes/account.register.tsx
+++ frontend/app/routes/account.register.tsx
@@ -1,13 +1,45 @@
-import { Link } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 
 export default function Register() {
   return (
-    <div>
-      <p>新規登録</p>
-      <input type="text" placeholder="ユーザー名" />
-      <input type="email" placeholder="メールアドレス" />
-      <input type="password" placeholder="パスワード" />
-      <Link to="/token">新規登録</Link>
+    <div className="max-w-96">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">新規登録</CardTitle>
+          <CardDescription>dice-o-genのアカウントを作成</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="min-w-72">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-1.5">
+                <Label htmlFor="username">ユーザー名</Label>
+                <Input id="username" type="text" />
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <Label htmlFor="email">メールアドレス</Label>
+                <Input id="email" type="email" />
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <Label htmlFor="password">パスワード</Label>
+                <Input id="password" type="password" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Button type="submit">新規登録</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }

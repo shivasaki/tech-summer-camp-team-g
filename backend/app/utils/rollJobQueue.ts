9786a7a487ea1fff890f988321538af38958e2fd
YoujinTea <takeanhime0126@gmail.com>
2024-09-16 08:48:20 +0900

    ロールジョブキューでのフェッチ処理を記述

--- backend/app/utils/rollJobQueue.ts
+++ backend/app/utils/rollJobQueue.ts
@@ -1,12 +1,41 @@
 import Queue from "./queue";
+import axios from "axios";
+import { query } from "./db";
 
 const rollJobQueue = new Queue();
+const rollUrl = process.env.DICEND_API_ROLL_URL;
+const statusUrl = process.env.DICEND_API_STATUS_URL;
 
 const rollRequestJob = async (sessionId: string): Promise<void> => {
     console.log(`Request roll session ${sessionId}`);
-    // TODO: セッションが完了するまで1秒ごとにAPIを叩く処理を後で実装
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    console.log(`Rolled session ${sessionId}`);
+    await axios.post(`${rollUrl}`);
+
+    // セッションの状態を更新
+    await query(
+        `UPDATE roll_session SET rolled_at = $1 WHERE id = $2`,
+        [new Date(), sessionId]
+    );
+
+    // セッションが完了するまで1秒ごとに結果を聞く
+    while (true) {
+        const response = await axios.get(`${statusUrl}`);
+        if (response.data.status === "completed") {
+            break;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        // 結果をDBに保存
+        await query(
+            `UPDATE user_session SET result = $1 WHERE session_id = $2`,
+            [response.data.result, sessionId]
+        );
+    }
+
+    // セッションの状態を更新
+    await query(
+        `UPDATE roll_session SET completed_at = $1 WHERE id = $2`,
+        [new Date(), sessionId]
+    );
 };
 
 export const addRollJob = (sessionId: string): void => {
